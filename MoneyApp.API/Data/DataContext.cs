aa56e663974cab11c2747d1ea1e4baecd0d14489
Wei Chen <wchen0530@gmail.com>
2020-07-05 22:36:22 -0400

    Initial Commit

---
+++ MoneyApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using MoneyApp.API.Models;
+
+namespace MoneyApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
