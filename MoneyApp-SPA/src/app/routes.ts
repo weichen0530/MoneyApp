25e07f355ca9f10c9df908ebcfd17322921a3668
Wei Chen <wchen0530@gmail.com>
2020-07-21 22:29:11 -0400

    added routing & route guard

---
+++ MoneyApp-SPA/src/app/routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { InventoryListComponent } from './inventory-list/inventory-list.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+export const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent },
+            { path: 'message', component: MessagesComponent },
+            { path: 'lists', component: ListsComponent },
+            { path: 'customer', component: CustomerListComponent },
+            { path: 'inventory', component: InventoryListComponent }
+        ]
+    },
+    { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
